dd5c4763b0273110a3230111e6ee7a7cb0c24659
Mauricio.M <m.morocho1196@gmail.com>
2023-04-22 14:43:25 -0500

    Fetching a single restaurant

--- app/restaurant/[restaurant-slug]/page.tsx
+++ app/restaurant/[restaurant-slug]/page.tsx
@@ -1,4 +1,3 @@
-import RestaurantHeader from "../components/RestaurantHeader";
 import RestaurantNavBar from "../components/RestaurantNavBar";
 import RestaurantTitle from "../components/RestaurantTitle";
 import RestaurantRating from "../components/RestaurantRating";
@@ -6,8 +5,41 @@ import RestaurantDescription from "../components/RestaurantDescription";
 import RestaurantImages from "../components/RestaurantImages";
 import RestaurantReviews from "../components/RestaurantReviews";
 import RestaurantReservationCard from "../components/RestaurantReservationCard";
+import { PrismaClient } from "@prisma/client";
 
-const RestaurantDetails = () => {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    }
+  });
+
+  if( !restaurant) {
+    throw new Error();
+  }
+
+  return restaurant;
+}
+
+const RestaurantDetails = async ({params}: {params: {'restaurant-slug': string}}) => {
+  const restaurant = await fetchRestaurantBySlug(params["restaurant-slug"]);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
