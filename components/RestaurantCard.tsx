6f4c86ace0f433aed5afefa94d52508fd57d8188
Mauricio.M <m.morocho1196@gmail.com>
2023-04-30 14:45:23 -0500

    Formatted RestaurantCard

--- components/RestaurantCard.tsx
+++ components/RestaurantCard.tsx
@@ -3,18 +3,14 @@ import Link from "next/link";
 import Price from "./Price";
 
 interface Props {
-  restaurant: RestaurantCardType
+  restaurant: RestaurantCardType;
 }
 //export dafault function RestaurantCard({ hello }: Props) {
 const RestaurantCard: React.FunctionComponent<Props> = ({ restaurant }) => {
   return (
     <Link href={`/restaurant/${restaurant.slug}`}>
       <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-        <img
-          src={restaurant.main_image}
-          alt=""
-          className="w-full h-36"
-        />
+        <img src={restaurant.main_image} alt="" className="w-full h-36" />
         <div className="p-1">
           <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
@@ -23,7 +19,7 @@ const RestaurantCard: React.FunctionComponent<Props> = ({ restaurant }) => {
           </div>
           <div className="flex text-reg font-light capitalize">
             <p className=" mr-3">{restaurant.cuisine.name}</p>
-            <Price price={restaurant.price}/>
+            <Price price={restaurant.price} />
             <p>{restaurant.location.name}</p>
           </div>
           <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
