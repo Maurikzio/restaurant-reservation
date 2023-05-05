a830965ca9d37c9fb15b442f55d3efe59d82f179
Mauricio.M <m.morocho1196@gmail.com>
2023-05-04 19:28:43 -0500

    Validating users input

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,4 +1,50 @@
-export async function POST(request: Request, res: Response) {
+import validator from "validator";
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request, response: Response) {
+
+  const { firstName, lastName, email, phone, city, password } = await request.json();
+
+  const errors: string[] = [];
+
+  const validationSchema = [
+    {
+      valid: validator.isLength(firstName, { min: 1, max: 20 }),
+      errorMessage: "Firstname is invalid",
+    },
+    {
+      valid: validator.isLength(lastName, { min: 1, max: 20 }),
+      errorMessage: "Lastname is invalid",
+    },
+    {
+      valid: validator.isEmail(email),
+      errorMessage: "Email is invalid",
+    },
+    {
+      valid: validator.isMobilePhone(phone),
+      errorMessage: "Phone is invalid",
+    },
+    {
+      valid: validator.isLength(city, { min: 1, max: 20 }),
+      errorMessage: "City is invalid",
+    },
+    {
+      valid: validator.isStrongPassword(password),
+      errorMessage: "Password is not strong enough",
+    }
+  ];
+
+  validationSchema.forEach(check => {
+    if(!check.valid) {
+      errors.push(check.errorMessage)
+    }
+  });
+
+  if(errors.length) {
+    return NextResponse.json({errorMessage: errors[0]}, {status: 400})
+  }
+
+
   return new Response("Hello there", {
     status: 200,
   })
