9de2281dae9bbc50a245e08164b635dcd15e624f
Mauricio.M <m.morocho1196@gmail.com>
2023-05-07 21:12:11 -0500

    Updating signIn fn in useAuth hook

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,14 +1,32 @@
+import { AuthenticationContext } from "@/app/context/AuthContext";
 import axios from "axios";
+import { useContext } from "react";
 
 const useAuth = () => {
+  const { data, error, loading, setAuthState } = useContext(AuthenticationContext);
+
   const signIn = async ({ email, password }: {email: string, password: string}) => {
+    setAuthState({
+      data: null, 
+      error: null, 
+      loading: true,
+    })
     try {
       const response = await axios.post("http://localhost:3000/api/auth/signin", {
         email, 
         password
       });
-    } catch(err) {
-      console.log(err);
+      setAuthState({
+        data: response.data, 
+        error: null, 
+        loading: false,
+      })
+    } catch(err: any) {
+      setAuthState({
+        data: null,
+        error: err.response.data.errorMessage,
+        loading: false,
+      })
     }
   };
 
