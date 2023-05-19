2afa30e3fb0b7774607464b2b2aae93966521e95
Mauricio.M <m.morocho1196@gmail.com>
2023-05-18 20:41:27 -0500

    Rendering the PartySize options

--- app/restaurant/components/RestaurantReservationCard.tsx
+++ app/restaurant/components/RestaurantReservationCard.tsx
@@ -1,16 +1,20 @@
 "use client";
+import { partySize } from "../../../data";
 
 const RestaurantReservationCard = () => {
   return (
-    <div className="fixed w-[15%] bg-white rounded p-3 shadow">
+    <div className="fixed w-[25%] bg-white rounded p-3 shadow">
       <div className="text-center border-b pb-2 font-bold">
         <h4 className="mr-7 text-lg">Make a Reservation</h4>
       </div>
       <div className="my-3 flex flex-col">
         <label htmlFor="">Party size</label>
         <select name="" className="py-3 border-b font-light" id="">
-          <option value="">1 person</option>
-          <option value="">2 people</option>
+          {partySize.map((item) => (
+            <option value="" key={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
       </div>
       <div className="flex justify-between">
---
+++ data/partySize.ts
@@ -0,0 +1,42 @@
+export const partySize = [
+  {
+    value: 1,
+    label: '1 Person'
+  },
+  {
+    value: 2,
+    label: '2 People'
+  },
+  {
+    value: 3,
+    label: '3 People'
+  },
+  {
+    value: 4,
+    label: '4 People'
+  },
+  {
+    value: 5,
+    label: '5 People'
+  },
+  {
+    value: 6,
+    label: '6 People'
+  },
+  {
+    value: 7,
+    label: '7 People'
+  },
+  {
+    value: 8,
+    label: '8 People'
+  },
+  {
+    value: 9,
+    label: '9 People'
+  },
+  {
+    value: 10,
+    label: '10 People'
+  },
+]
